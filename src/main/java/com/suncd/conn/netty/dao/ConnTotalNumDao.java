9d9c1e0fc3b78202c4ecd3e8c76d318f3c7341eb
qust <13214123320@163.com>
2018-10-31 09:19:48 +0800

    统计

---
+++ src/main/java/com/suncd/conn/netty/dao/ConnTotalNumDao.java
@@ -0,0 +1,17 @@
+package com.suncd.conn.netty.dao;
+
+import com.suncd.conn.netty.entity.ConnTotalNum;
+
+public interface ConnTotalNumDao {
+    int deleteByPrimaryKey(String id);
+
+    int insert(ConnTotalNum record);
+
+    int insertSelective(ConnTotalNum record);
+
+    ConnTotalNum selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(ConnTotalNum record);
+
+    int updateByPrimaryKey(ConnTotalNum record);
+}
